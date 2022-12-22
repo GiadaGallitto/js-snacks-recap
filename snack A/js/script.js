16f613f61373a98341990c49bc61346e16c03032
GiadaGallitto <gallitto.giada@gmail.com>
2022-12-22 16:40:06 +0100

    Fix code

--- snack A/js/script.js
+++ snack A/js/script.js
@@ -1,42 +1,34 @@
-/* Creare un input che permetta all'utente di inserire un messaggio e aggiungerlo 
-ad una conversazione (tipo whatsapp).
-Dopo averlo aggiunto chiedere all'API una frase random (attraverso random/sentence) 
-aggiungendo anch'essa al thread,
- differenziando i messaggi utente da quelli del computer. */
+/** Chiedere all'utente quanti elementi vuole includere nell'array.
+Generare attraverso l'API (array/integers) un array contenente N numeri compresi tra 1 e 100.
+Stampare a schermo la somma dei numeri generati. */
 
 const {createApp} = Vue;
 
 createApp({
     data(){
         return{
-            messages: [],
-            inputText: null,
+            numbers: [],
+            inputNumber: 0,
         }
     },
 
     methods: {
-        addNewMessage(content){
-            if(content != null && content != ""){
-                this.messages.push({
-                    subject : "Tu:", message : this.inputText, status : "sent"
-                });
-                this.getRandomAnswer();
-                this.inputText = "";
+        addUserNumbers(inputNumber){
+            if(inputNumber != null && inputNumber != "" && (!inputNumber < 1 )){
+                this.getRandomNumbers(inputNumber)
             }
         },
 
-        getRandomAnswer(){
-            let computerMessage = "";
-            axios.get(`https://flynn.boolean.careers/exercises/api/random/sentence`)
-            .then((response) => {
-                computerMessage = response.data.response;
-                console.log(computerMessage);
-                setTimeout(() => {
-                    this.messages.push({
-                        subject : "Computer:", message : `${computerMessage}` , status : "received"
-                    });
-                }, 1000)
-            });
+        getRandomNumbers(num){
+            let randomNumber = "";
+            for(i=0; i < num; i++){
+                axios.get(`https://flynn.boolean.careers/exercises/api/random/sentence`)
+                .then((response) => {
+                    randomNumber = response.data.response;
+                    console.log(randomNumber);
+                    this.numbers.push(randomNumber)
+                });
+            }
         }
     },
     
