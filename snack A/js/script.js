bfa66688c14869dac2c8a2313fc0321346ab7e34
GiadaGallitto <gallitto.giada@gmail.com>
2022-12-22 16:44:15 +0100

    Fix code position

--- snack A/js/script.js
+++ snack A/js/script.js
@@ -1,35 +1,43 @@
-/** Chiedere all'utente quanti elementi vuole includere nell'array.
-Generare attraverso l'API (array/integers) un array contenente N numeri compresi tra 1 e 100.
-Stampare a schermo la somma dei numeri generati. */
+/* Creare un input che permetta all'utente di inserire un messaggio e aggiungerlo 
+ad una conversazione (tipo whatsapp).
+Dopo averlo aggiunto chiedere all'API una frase random (attraverso random/sentence) 
+aggiungendo anch'essa al thread,
+ differenziando i messaggi utente da quelli del computer. */
 
-const {createApp} = Vue;
+const { createApp } = Vue;
 
 createApp({
-    data(){
-        return{
-            numbers: [],
-            inputNumber: 0,
+    data() {
+        return {
+            messages: [],
+            inputText: null,
         }
     },
 
     methods: {
-        addUserNumbers(inputNumber){
-            if(inputNumber != null && inputNumber != "" && (!inputNumber < 1 )){
-                this.getRandomNumbers(inputNumber)
+        addNewMessage(content) {
+            if (content != null && content != "") {
+                this.messages.push({
+                    subject: "Tu:", message: this.inputText, status: "sent"
+                });
+                this.getRandomAnswer();
+                this.inputText = "";
             }
         },
 
-        getRandomNumbers(num){
-            let randomNumber = "";
-            for(i=0; i < num; i++){
-                axios.get(`https://flynn.boolean.careers/exercises/api/random/sentence`)
+        getRandomAnswer() {
+            let computerMessage = "";
+            axios.get(`https://flynn.boolean.careers/exercises/api/random/sentence`)
                 .then((response) => {
-                    randomNumber = response.data.response;
-                    console.log(randomNumber);
-                    this.numbers.push(randomNumber)
+                    computerMessage = response.data.response;
+                    console.log(computerMessage);
+                    setTimeout(() => {
+                        this.messages.push({
+                            subject: "Computer:", message: `${computerMessage}`, status: "received"
+                        });
+                    }, 1000)
                 });
-            }
         }
     },
-    
+
 }).mount(`#app`)
